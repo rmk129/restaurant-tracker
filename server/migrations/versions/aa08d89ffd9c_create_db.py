e202ef9748dd5970f56a0ecc0ae271a663cccb17
Romil <romil.m.kothari@gmail.com>
2024-03-03 15:49:33 -0500

    created models initial

---
+++ server/migrations/versions/aa08d89ffd9c_create_db.py
@@ -0,0 +1,24 @@
+"""Create DB
+
+Revision ID: aa08d89ffd9c
+Revises: 
+Create Date: 2024-03-03 15:30:55.040363
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'aa08d89ffd9c'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
--- server/models.py
+++ server/models.py
@@ -4,3 +4,38 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from config import db
 
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String)
+
+
+    def __repr__(self):
+        return f'<User {self.id}, {self.username}, {self.password}>'
+    
+
+class Review(db.Model, SerializerMixin):
+    __tablename__ = 'reviews'
+
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer)
+    message = db.Column(db.String, nullable=False)
+
+    def __repr__(self):
+        return f'<Review {self.id}, {self.score}, {self.message}>'
+    
+
+class Restaurant(db.Model, SerializerMixin):
+    __tablename__ = 'restaurants'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.string)
+    cuisine = db.Column(db.string)
+
+    def __repr__(self):
+        return f'<Restaurant {self.id}, {self.name}, {self.cuisine}>'
+
+
+    
